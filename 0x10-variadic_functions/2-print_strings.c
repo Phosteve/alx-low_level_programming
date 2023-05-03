5826725c87b18910d8ec2745f4115a05c1e4dd9d
Phosteve <phostevesachieng@gmail.com>
2023-05-03 00:05:30 -0700

    Updated

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,13 +3,13 @@
 #include <stdarg.h>
 
 /**
- * print_strings - Prints strings, followed by a new line
- * @separator: The string to be rinted between strings.
- * @n: The number of stings passed to the function.
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between strings.
+ * @n: The number of strings passed to the function.
  * @...: A variable number of strings to be printed.
  *
  * Description: If separator is NULL, it is not printed.
- *         If one of the strings if NULL, (nil) is printed instead.
+ *              If one of the strings if NULL, (nil) is printed instead.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -21,15 +21,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (index = 0; index < n; index++)
 	{
-		st = va_arg(strings, char*);
+		str = va_arg(strings, char *);
 
 		if (str == NULL)
 			printf("(nil)");
 		else
-			printf("%s",  str);
+			printf("%s", str);
+
 		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
+
 	printf("\n");
 
 	va_end(strings);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,9 @@
-#ifndef_VARIADIC_H_
-#define_VARIADIC_H_
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
-int_putchar(char c);
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n,...);
-void_print_all(const char * const format, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
